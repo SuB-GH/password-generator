7fc0f21714c0912ac38e483109962ca361032147
Susan Brown <brown684408@gmail.com>
2022-07-21 15:45:56 -0500

    add widow confirms and arrays

--- script.js
+++ script.js
@@ -2,65 +2,67 @@ var upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N
 var lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numeric = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '-', '.', '~', '|', '<', '>', '=', '-', '_', '/', ':', ';', '?', '[', ']', '{', '}', '~'];
-var passwordLength = 10;
-var password = "";
-
-
-for(var i = 0; i < upper.length; i++) {
-  for(var i = 0; i < lower.length; i++)
-  for(var i = 0; i < numeric.length; i++)
-  for(var i = 0; i < specChar.length; i++)
-  console.log(upper[i], lower[i], numeric[i], specChar[i]);
-
-}
-
+var passwordLength
+var containerArray = [];
 
 
 // button click generates window prompt with first question
 var generateBtn = document.querySelector("#generate");
 
+
+
 generateBtn.addEventListener("click", function () {
   // uppercase letters prompt
-  var upperCase = window.prompt("Would you like to include uppercase letters in your password?")
+  //var upperCase = window.prompt("Would you like to include uppercase letters in your password?")
+var password = ""
 
-  if (upperCase === "yes" || upperCase === "YES")
-    window.prompt("Ok. Your password will include uppercase letters.");
+  passwordLength = window.prompt("How many characters would you like your password to be? (Between 8-128)");
 
-  else if (upperCase === "no" || upperCase === "NO")
-    window.prompt("Ok. Your password will NOT include uppercase letters.");
+  var upperCase = window.confirm("Would you like to include uppercase letters in your password?")
 
-  //lowercase letters prompt
-  var lowerCase = window.prompt("Would you like to include lowercase letters in your password?")
+  if (upperCase) {
+    containerArray.push(...upper);
+   
+  }
+   
+    //lowercase letters prompt
+  var lowerCase = window.confirm("Would you like to include lowercase letters in your password?")
 
-  if (lowerCase === "yes" || lowerCase === "YES")
-    window.prompt("Ok. Your password will include lowercase letters.");
-
-  else if (lowerCase === "no" || lowerCase === "NO")
-    window.prompt("Ok. Your password will NOT include lowercase letters.");
+  if (lowerCase) {
+    containerArray.push(...lower);
+  }
 
   //numbers prompt
-  var numbers = window.prompt("Would you like to include numbers in your password?")
+  var numbers = window.confirm("Would you like to include numbers in your password?")
 
-  if (numbers === "yes" || numbers === "YES")
-    window.prompt("Ok. Your password will include numbers.");
-
-  else if (numbers === "no" || numbers === "NO")
-    window.prompt("Ok. Your password will NOT include numbers.");
+  if (numbers) {
+    containerArray.push(...numeric);
+  }
 
   //special characters prompt
-  var characters = window.prompt("Would you like to include special characters in your password?")
+  var characters = window.confirm("Would you like to include special characters in your password?")
 
-  if (characters === "yes" || characters === "YES")
-    window.prompt("Ok. Your password will include special characters.");
+  if (characters) {
+    containerArray.push(...specChar);
+  }
 
-  else if (characters === "no" || characters === "NO")
-    window.prompt("Ok. Your password will NOT include special characters.");
+  for(var i = 0; i < passwordLength; i++) {
+    var randomItem = containerArray[Math.floor(Math.random() * containerArray.length)];
+    password = password + randomItem
 
+  }
+
+  
+
+  console.log(password);
   // for (let i = 0; i = 5; i++) {
   //   text += "test number is " + i + "<br>";
   // };
 
 })
+
+
+
 
 
 // for(var i=0; i< = passwordLength; i++) {
@@ -78,3 +80,41 @@ generateBtn.addEventListener("click", function () {
 
 // var randomItem = specChar[Math.floor(Math.random() * specChar.length)];
 // console.log(randomItem4);
+
+
+// var randomChar = function(min, max) {
+//   var value = Math.floor(Math.random() * 12);
+
+//   return value;
+  
+// }
+// randomChar();
+// console.log(randomChar);
+
+
+// for(var i = 0; i < upper.length; i++) {
+//   for(var i = 0; i < lower.length; i++)
+//   for(var i = 0; i < numeric.length; i++)
+//   for(var i = 0; i < specChar.length; i++)
+//   console.log(upper[i], lower[i], numeric[i], specChar[i]);
+
+
+// // STARTER CODE BEGIN
+// // Assignment code here
+// // Get references to the #generate element
+// 
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+// STARTER CODE END
